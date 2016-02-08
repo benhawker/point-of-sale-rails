8ca201e040d351ee4ba2fb73350d9e4e9ed1bead
Ben <ben.c.hawker@gmail.com>
2016-02-08 18:21:07 +0100

    Comments out a few things

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -2,40 +2,40 @@ class OrderItemsController < ApplicationController
 
 	before_action :get_order
 
-  def index
-    @order_items = @order.line_items.all
-  end
+  # def index
+  #   @order_items = @order.line_items.all
+  # end
 
-  def show
-    @order_item = @order.line_items.find(params[:id])
-  end
+  # def show
+  #   @order_item = @order.line_items.find(params[:id])
+  # end
 
-  def new
-    @order_item = OrderItem.new
-  end
+  # def new
+  #   @order_item = OrderItem.new
+  # end
 
-  def create
-    @order_item = @order.line_items.find(params[:id])
-    if @line_item.save(order_items_params)
-    	flash[:notices] = ["New order item successfully created"]
-      redirect_to order_path(@order)
-    else
-			render 'new'
-    end
-  end
+  # def create
+  #   @order_item = @order.order_items.find(params[:id])
+  #   if @order_item.save(order_items_params)
+  #   	flash[:notices] = ["New order item successfully created"]
+  #     redirect_to order_path(@order)
+  #   else
+		# 	render 'new'
+  #   end
+  # end
 
-  def update
-    @order_item = @order.line_items.find(params[:id])
-    if @line_item.update(order_items_params)
-    	flash[:notices] = ["Order item successfully updated"]
-      redirect_to order_path(@order)
-    else
-			render 'new'
-    end
-  end
+  # def update
+  #   @order_item = @order.line_items.find(params[:id])
+  #   if @line_item.update(order_items_params)
+  #   	flash[:notices] = ["Order item successfully updated"]
+  #     redirect_to order_path(@order)
+  #   else
+		# 	render 'new'
+  #   end
+  # end
 
   def destroy
-    @order_item = @order.line_items.find(params[:id])
+    @order_item = @order.order_items.find(params[:id])
     if @order_item.destroy
       flash[:notices] = ['Order Item was successfully removed']
       redirect_to order_path(@order)
