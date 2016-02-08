6513693da1da5563c65dc872ba05b6aef814d2fa
Ben <ben.c.hawker@gmail.com>
2016-02-08 18:21:30 +0100

    To come back to

--- app/assets/javascripts/addOrRemoveProductsFromOrder.js
+++ app/assets/javascripts/addOrRemoveProductsFromOrder.js
@@ -1,25 +1,26 @@
-$(document).ready(function(){
-  $("#addNewProduct").click(function() {
-    $("#order-items-div").append(createNewInputElement($("#order-items-div")));
-  });
-});
+// $(document).ready(function() {
+//   $("#addNewProduct").click(function() {
+//     $("#items-div").append(createNewInputElement($("#items-div")));
+//   });
+// });
 
-function createNewInputElement(form) {
-  var newIndex = $("#order-items-div").children('input#order-items-entry').length;
-  var newInput = $("#order-items-entry").clone().attr('name', generateNewInputName(newIndex));
-  newInput.val('');
-  return newInput;
-};
+// function createNewInputElement(form) {
+//   var newIndex = $("#items-div").children('input.item-box').length;
+//   var newInput = $(".item-box").clone().attr('name', generateNewInputName(newIndex));
+//   newInput.val('');
+//   return newInput;
+// };
 
-function generateNewInputName(idx) {
-  return "order[order_items_attributes][" + idx + "][product_id]"
-};
+// function generateNewInputName(idx) {
+//   return "order[order_items_attributes][" + idx + "][product_id]"
+// };
 
 
-$(document).ready(function(){
-  $("#deleteLastProduct").click(function() {
-    if ($("#order-items-div input").length > 1) {
-     $("#order-items-div input:last-child").remove();
-  }
-  });
-});
+// $(document).ready(function(){
+//   $("#deleteLastProduct").click(function() {
+//     if ($("#items-div input").length > 1) {
+//      $("#items-div input:last-child").remove();
+//   }
+//   });
+// });
+
