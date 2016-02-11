8d55ad35c522236bb989fe4005e6c82e94cde480
Ben <ben.c.hawker@gmail.com>
2016-02-11 21:57:00 +0100

    Outline of some scrappy feature specs

--- spec/features/orders_feature_spec.rb
+++ spec/features/orders_feature_spec.rb
@@ -2,5 +2,16 @@ require 'rails_helper'
 
 feature "Orders" do
 
+	let(:category) { FactoryGirl.create(:category, name: "Test Category") }
+	let(:product) { FactoryGirl.create(:product) }
+	let(:second_product) { FactoryGirl.create(:product, name: "Second Product") }
+
+	it 'can create a new order', js: true do
+	  visit root_path
+	  click_link 'New Order'
+	  fill_in 'order[customer]',with: "My new category"
+	  click_button 'Create Category'
+	  expect(page).to have_content("My new category")
+	end
 
 end
